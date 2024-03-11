a45ef982634ad222dfc19050321bdd15b91e2a72
kuhusinha9 <kuhusinha92@gmail.com>
2024-03-11 15:32:37 +0100

    Base code with temp student data

---
+++ json_converter.py
@@ -0,0 +1,45 @@
+import json
+ 
+# Opening JSON file
+f = open('students.json')
+ 
+# returns JSON object as 
+# a dictionary
+data = json.load(f)
+
+def get_student_data(name):
+    try:
+        temp=data['students']['student'][name]
+        
+        # base script
+        base = f"{name} is {temp['age']}. They like {temp['hobby']}. They like reading {temp['favGenre']} books. "
+
+        # siblings script
+        num= len(temp['siblings'])
+        s = ""
+        if  num == 1:
+            s = f"They have a sibling called {temp['siblings'][0]['name']} who is {temp['siblings'][0]['age']} years old. "
+        elif num > 1:
+            s = f"They have {num} siblings- "
+            for i in range(num - 1):
+                s = s + temp['siblings'][i]['name'] + " is " + temp['siblings'][i]['age'] + ", "
+            s = s + "and " + temp['siblings'][num-1]['name'] + " is " + temp['siblings'][num-1]['age'] + ". "
+
+        # pets script
+        num= len(temp['pet'])
+        p = ""
+        if  num == 1:
+            p = f"They have a {temp['pet'][0]['animal']} called {temp['pet'][0]['name']}. "
+        elif num > 1:
+            p = f"They have {num} pets- "
+            for i in range(num - 1):
+                p = p + "a "+ temp['pet'][i]['animal'] + " called " + temp['pet'][i]['name'] + ", "
+            p = p + "and a " + temp['pet'][num-1]['animal'] + " called " + temp['pet'][num-1]['name']  + ". "
+
+        final= base + s + p
+        return final
+    except KeyError:
+        return f"You have no information about {name}."
+
+# Closing file
+f.close()
--- main.py
+++ main.py
@@ -1,2 +1,26 @@
+import os
+import openai
+from openai import OpenAI
+from dotenv import load_dotenv
+from json_converter import get_student_data
 
-test
+load_dotenv()
+client = OpenAI(
+  api_key=os.environ["OPENAI_API_KEY"]
+)
+
+data = get_student_data("Maria")
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a teaching assistant for an elementary school classroom. You write lists of questions that help the students, who are eight to eleven years old, connect with what they are reading."},
+    {"role": "system", "content": "Maria is reading Anne of Green Gables. "},
+    {"role": "system", "content": data},
+    {"role": "system", "content": "What questions should you ask her?"}
+  ],
+  temperature= 1
+
+)
+
+print(completion.choices[0].message.content)
