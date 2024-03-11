844092ef17cd43ab393823002b7f66c20d8a1149
kuhusinha9 <kuhusinha92@gmail.com>
2024-03-11 16:01:23 +0100

    chapters now work

--- main.py
+++ main.py
@@ -9,15 +9,21 @@ client = OpenAI(
   api_key=os.environ["OPENAI_API_KEY"]
 )
 
-data = get_student_data("Maria")
+name = input("Enter the name of the student. \n")
+data = get_student_data(name)
+
+reading=input("Enter the filename (with extention) that the student har read \n")
+with open(f"stories/{reading}", 'r', encoding='utf-8') as f:
+    text= f.read()
 
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
     {"role": "system", "content": "You are a teaching assistant for an elementary school classroom. You write lists of questions that help the students, who are eight to eleven years old, connect with what they are reading."},
-    {"role": "system", "content": "Maria is reading Anne of Green Gables. "},
+    {"role": "system", "content": text},
+    {"role": "system", "content": f"{name} has read this text. "},
     {"role": "system", "content": data},
-    {"role": "system", "content": "What questions should you ask her?"}
+    {"role": "system", "content": f"Write 5 questions for {name}."}
   ],
   temperature= 1
 
